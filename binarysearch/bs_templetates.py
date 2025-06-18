03f73187a98f74f881b5a90936ecb7b3e0162644
Gemechu Alemu <alemugemechu44@gmail.com>
2025-06-18 19:03:53 +0300

    lower and upper bound

--- binarysearch/bs_templetates.py
+++ binarysearch/bs_templetates.py
@@ -10,3 +10,23 @@ def binary_search(arr, target):
         else:
             right = mid - 1
     return -1  # not found
+# lower bound
+def lower_bound(arr, target):
+    left, right = 0, len(arr)
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+    return left  # index of first element >= target
+# upper bound
+def upper_bound(arr, target):
+    left, right = 0, len(arr)
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] <= target:
+            left = mid + 1
+        else:
+            right = mid
+    return left  # index of first element > target
